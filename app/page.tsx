1b23a4cab7bc2a2d35eef188e7a7317b8f98548f
thiagoDOTjpeg <thiago.gritti12@gmail.com>
2025-10-31 17:36:05 -0300

    feat: mudança de nome de subtítulo

--- app/page.tsx
+++ app/page.tsx
@@ -102,7 +102,7 @@ export default function Home() {
             <p
               className="text-lg sm:text-xl mb-10"
               style={{
-                color: "var(--muted)",
+                // color: "var(--muted)",
                 textShadow: "2px 2px 4px rgba(0,0,0,0.9)",
               }}
             >
